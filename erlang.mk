15c7335dff7cd5e8ccd648a990b27751937e7be7
Loïc Hoguin <essen@ninenines.eu>
2018-05-24 16:44:50 +0200

    Update Erlang.mk to remove a warning on OSX
    This doesn't solve the real issues but small steps.

--- erlang.mk
+++ erlang.mk
@@ -6015,7 +6015,7 @@ ifeq ($(PLATFORM),msys2)
 	CXXFLAGS ?= -O3 -finline-functions -Wall
 else ifeq ($(PLATFORM),darwin)
 	CC ?= cc
-	CFLAGS ?= -O3 -std=c99 -arch x86_64 -finline-functions -Wall -Wmissing-prototypes
+	CFLAGS ?= -O3 -std=c99 -arch x86_64 -Wall -Wmissing-prototypes
 	CXXFLAGS ?= -O3 -arch x86_64 -Wall
 	LDFLAGS ?= -arch x86_64 -flat_namespace -undefined suppress
 	C_SRC_SHARED_FLAG = -bundle
